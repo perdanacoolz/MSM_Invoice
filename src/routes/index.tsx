9bba5cfc0856fd89f165d06179bce09f0ba6b24f
perdana <perdanarobert9@gmail.com>
2026-01-13 13:16:11 +0700

    test

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute, Navigate } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return <Navigate to="/dashboard" />
+}
